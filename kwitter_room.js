1cfdd63583f6e28d9b797e1d3067bff8906253c9
Vivan Bhargava <78213755+vivanbhargava@users.noreply.github.com>
2021-10-27 14:24:33 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,17 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyDoRnwTRBwNPeLMOHH_2T_hcNBoQ8WX2YM",
+
+    authDomain: "kwitter-a3504.firebaseapp.com",
+
+    projectId: "kwitter-a3504",
+
+    storageBucket: "kwitter-a3504.appspot.com",
+
+    messagingSenderId: "179497382661",
+
+    appId: "1:179497382661:web:f729712dbdef7bc26a7d6c"
+
+};
+
+firebase.initializeApp(firebaseConfig);
